e607cf91e78193f6d53b819c08d8e98dbd788fd6
AidanBrasseur <aidanbrasseur@gmail.com>
2019-05-25 13:02:29 -0400

    no message

---
+++ Foreign Agent/Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+    public float moveSpeed = 0.02f;
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * moveSpeed);
+        m_Rigidbody.MoveRotation(m_Rotation);
+        m_Rigidbody.velocity = Vector3.zero;
+        m_Rigidbody.angularVelocity = Vector3.zero;
+
+    }
+
+}
