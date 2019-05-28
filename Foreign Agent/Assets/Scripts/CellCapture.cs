6f8c085e0951cdf744fe3cd5f5fab21bf88c0624
AidanBrasseur <aidanbrasseur@gmail.com>
2019-05-27 22:32:06 -0400

    Added capture points and rough level design

---
+++ Foreign Agent/Assets/Scripts/CellCapture.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellCapture : MonoBehaviour
+{
+    public GameObject player;
+    private bool startCap = false;
+    public static float capTime = 5f;
+    [HideInInspector]
+    public static float currTime;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            startCap = true;
+            currTime = capTime;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startCap)
+        {
+            currTime -= Time.deltaTime;
+            if (currTime <= 0)
+            {
+                Debug.Log("Capped");
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject == player)
+        {
+            startCap = false;
+            currTime = capTime;
+        }
+    }
+}
