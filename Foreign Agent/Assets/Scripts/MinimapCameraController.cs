b5fe3a8a0645eb8becc2ab6fac4e530c90f26ab9
Sky Li <skylimail42@gmail.com>
2019-06-11 21:23:05 -0400

    added very basic minimap

---
+++ Foreign Agent/Assets/Scripts/MinimapCameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+	}
+}
