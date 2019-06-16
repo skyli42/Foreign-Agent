5c05aa95435e4f043612089be64d1a46f8a6abec
AidanBrasseur <aidanbrasseur@gmail.com>
2019-06-16 16:18:16 -0400

    Added pause menu. Keyboard menu navigation is wrong for some reason though.

---
+++ Foreign Agent/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject menu;
+    private bool paused;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = togglePause();
+            if (paused)
+                menu.GetComponent<Pause>().Display();
+            else
+                menu.GetComponent<Pause>().ReturnToGame();
+        }
+    }
+    
+    public bool togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            return (true);
+        }
+    }
+}
