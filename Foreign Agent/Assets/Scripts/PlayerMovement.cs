a9657c179c909c70283fabee297244f6f992d2d4
Sky Li <skylimail42@gmail.com>
2019-06-21 01:55:24 -0400

    new model improted

--- Foreign Agent/Assets/Scripts/PlayerMovement.cs
+++ Foreign Agent/Assets/Scripts/PlayerMovement.cs
@@ -63,7 +63,7 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
-
+		Debug.Log(dashStart);
         if (slider.value == 1)
         {
             sliderTimer += Time.deltaTime;
