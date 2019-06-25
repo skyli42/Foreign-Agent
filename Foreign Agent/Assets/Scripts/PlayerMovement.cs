fb9cf527e44ad406562dbdc7cef8c577fc24ad36
Sky Li <skylimail42@gmail.com>
2019-06-24 22:19:25 -0400

    added in running

--- Foreign Agent/Assets/Scripts/PlayerMovement.cs
+++ Foreign Agent/Assets/Scripts/PlayerMovement.cs
@@ -1,56 +1,118 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float turnSpeed = 20f;
+	public float turnSpeed = 20f;
+	public float moveSpeed = 0.05f;
+	private float originalSpeed;
+	public float maxDash = 2f;
+	private float dash = 2f;
+	public float dashSpeed;
 
-    Animator m_Animator;
-    Rigidbody m_Rigidbody;
-    AudioSource m_AudioSource;
-    Vector3 m_Movement;
-    Quaternion m_Rotation = Quaternion.identity;
+	public Slider slider;
+	private bool dashStart = false;
+	private float sliderTimer = 0;
+	public float regenRate = 0.5f;
+	Quaternion fixedRotation;
 
-    void Start ()
-    {
-        m_Animator = GetComponent<Animator> ();
-        m_Rigidbody = GetComponent<Rigidbody> ();
-        m_AudioSource = GetComponent<AudioSource> ();
-    }
 
-    void FixedUpdate ()
-    {
-        float horizontal = Input.GetAxis ("Horizontal");
-        float vertical = Input.GetAxis ("Vertical");
-        
-        m_Movement.Set(horizontal, 0f, vertical);
-        m_Movement.Normalize ();
+	Animator m_Animator;
+	Rigidbody m_Rigidbody;
+	AudioSource m_AudioSource;
+	Vector3 m_Movement;
+	Quaternion m_Rotation = Quaternion.identity;
 
-        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
-        bool isWalking = hasHorizontalInput || hasVerticalInput;
-        m_Animator.SetBool ("IsWalking", isWalking);
-        
-        //if (isWalking)
-        //{
-        //    if (!m_AudioSource.isPlaying)
-        //    {
-        //        m_AudioSource.Play();
-        //    }
-        //}
-        //else
-        //{
-        //    m_AudioSource.Stop ();
-        //}
 
-        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
-        m_Rotation = Quaternion.LookRotation (desiredForward);
-    }
 
-    void OnAnimatorMove ()
-    {
-        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
-        m_Rigidbody.MoveRotation (m_Rotation);
-    }
+	void Start()
+	{
+		m_Animator = GetComponent<Animator>();
+		m_Rigidbody = GetComponent<Rigidbody>();
+		m_AudioSource = GetComponent<AudioSource>();
+		originalSpeed = moveSpeed;
+		dash = maxDash;
+		fixedRotation = slider.GetComponentInParent<Canvas>().transform.rotation;
+	}
+
+	void FixedUpdate()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		m_Movement.Set(horizontal, 0f, vertical);
+		m_Movement.Normalize();
+
+		bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+		bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+		bool isWalking = hasHorizontalInput || hasVerticalInput;
+	
+		m_Animator.SetBool("IsWalking", isWalking);
+		m_Animator.SetBool("IsRunning", dashStart);
+		Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+		m_Rotation = Quaternion.LookRotation(desiredForward);
+	}
+
+	void OnAnimatorMove()
+	{
+		m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+		m_Rigidbody.MoveRotation(m_Rotation);
+	}
+	private void LateUpdate()
+	{
+		slider.GetComponentInParent<Canvas>().transform.rotation = fixedRotation;
+	}
+	private void Update()
+	{
+		if (slider.value == 1)
+		{
+			sliderTimer += Time.deltaTime;
+			if (sliderTimer > 1)
+			{
+				slider.gameObject.SetActive(false);
+			}
+		}
+		if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
+		{
+			slider.gameObject.SetActive(true);
+			sliderTimer = 0;
+			dashStart = true;
+		}
+		else if ((Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift)) || (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0))
+		{
+			moveSpeed = originalSpeed;
+			dashStart = false;
+		}
+		if (dash <= 0)
+		{
+			dashStart = false;
+		}
+		if (dashStart)
+		{
+			dash -= Time.deltaTime;
+			moveSpeed = dashSpeed;
+			if (dash <= 0)
+			{
+				moveSpeed = originalSpeed;
+			}
+		}
+		else
+		{
+			if (dash < maxDash)
+			{
+				dash += regenRate * Time.deltaTime;
+				if (dash > maxDash)
+				{
+					dash = maxDash;
+				}
+			}
+		}
+		float complete = 1 - (maxDash - dash) / maxDash;
+		slider.value = complete;
+	}
 }
+
+
