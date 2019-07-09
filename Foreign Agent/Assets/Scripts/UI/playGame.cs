e2c2b019e0125096e5c280a5d735dcf6804d4b4d
AidanBrasseur <aidanbrasseur@gmail.com>
2019-07-09 18:57:16 -0400

    Separated loading screen objects for each level

--- Foreign Agent/Assets/Scripts/UI/playGame.cs
+++ Foreign Agent/Assets/Scripts/UI/playGame.cs
@@ -3,16 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class playGame : MonoBehaviour
 {
+
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1.0f;
     }
 
-    public void NextLevel()
+    public void NextLevel(GameObject loadingScreen)
     {
         int sceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-        LoadingScreen.Instance.Show(SceneManager.LoadSceneAsync(sceneIndex));
+        loadingScreen.GetComponent<LoadingScreen>().Show(SceneManager.LoadSceneAsync(sceneIndex));
         Time.timeScale = 1.0f;
     }
 }
