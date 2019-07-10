bc3513585ce6efeb0a51f17ad1e37a0eae7b0c3a
AidanBrasseur <aidanbrasseur@gmail.com>
2019-07-09 23:42:03 -0400

    Made loading screen pic and caption fade in with the animation.

---
+++ Foreign Agent/Assets/Scripts/fadeWithBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class fadeWithBackground : MonoBehaviour
+{
+    private Color newColour;
+    private Color backColour;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (GetComponent<Graphic>() != null)
+        {
+            newColour = GetComponent<Graphic>().color;
+            backColour = transform.parent.gameObject.GetComponent<Graphic>().color;
+            newColour.a = backColour.a;
+            GetComponent<Graphic>().color = newColour;
+        }
+        else
+        {
+
+            newColour = GetComponent<TextMeshProUGUI>().color;
+            backColour = transform.parent.gameObject.GetComponent<Graphic>().color;
+            newColour.a = backColour.a;
+            GetComponent<TextMeshProUGUI>().color = newColour;
+        }
+    }
+}
