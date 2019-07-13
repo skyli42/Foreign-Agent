3167a145890648354beff1c4ed3c25bba2234f35
skyli42 <skylimail42@gmail.com>
2019-07-13 00:39:30 -0400

    added random loading text

--- Foreign Agent/Assets/Scripts/LoadingScreen.cs
+++ Foreign Agent/Assets/Scripts/LoadingScreen.cs
@@ -1,7 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 public class LoadingScreen : MonoBehaviour
 {
+
+	public TextAsset txtFile;
+	public TextMeshProUGUI tutorialText;
+	private readonly Dictionary<int, List<int>> loadTextIndex = new Dictionary<int, List<int>> // level index --> possible text
+	{
+		{ 0, new List<int>{0}}, // MainMenu
+		{ 1, new List<int>{0, 1, 2, 3, 4, 5, 6}}, // Tutorial1
+		{ 2, new List<int>{0, 1, 2, 3, 4, 5, 6} }, //MacrophageIntro1
+		{ 3, new List<int>{0, 1, 2, 3, 4, 5, 6 } }, //MacrophageIntro2
+		{ 4, new List<int>{7, 8, 9, 10, 11, 12, 13} }, //BCellTutorial
+		{ 5, new List<int>{7, 8, 9, 10, 11, 12, 13} }, //BCellIntro
+		{ 6, new List<int>{7, 8, 9, 10, 11, 12, 13} }, //BCellIntro2
+		{ 7, new List<int>{14, 15, 16, 17} }, //TCellTutorial
+		{ 8, new List<int>{14, 15, 16, 17} }, //TCellIntro1
+		{ 9, new List<int>{14, 15, 16, 17} }, //TCellIntro2
+		{ 10, new List<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17} }, //1.2
+		{ 11, new List<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17} }, //1.3
+		{ 12, new List<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17} }, //1.4
+		{ 13, new List<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19} }, //2.1
+		{ 14, new List<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19} }, //2.2
+		{ 15, new List<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19} }, //2.3
+		{ 16, new List<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19} } //Vaccine?
+
+	};
     // Make sure the loading screen shows for at least 1 second:
     private const float MIN_TIME_TO_SHOW = 5f;
     // The reference to the current loading operation running in the background:
@@ -29,7 +57,7 @@ public class LoadingScreen : MonoBehaviour
     // Flag whether the fade out animation was triggered.
     private bool didTriggerFadeOutAnimation;
     public GameObject prevMenu;
-
+	private string[] loadingTexts;
     private void Awake()
     {
         // Singleton logic:
@@ -37,8 +65,25 @@ public class LoadingScreen : MonoBehaviour
         DontDestroyOnLoad(gameObject);
         Configure();
         Hide();
+		LoadText();
     }
-    private void Configure()
+	private void LoadText()
+	{
+		string[] texts = txtFile.text.Split('\n');
+		
+		int y = SceneManager.GetActiveScene().buildIndex;
+		loadingTexts = new string[loadTextIndex[y].Count];
+		for (int i=0; i<loadTextIndex[y].Count; i++)
+		{
+			loadingTexts[i] = texts[loadTextIndex[y][i]];
+		}
+		
+		int index = (int)Random.Range(0, loadingTexts.Length);
+		Debug.Log(loadingTexts[index]);
+		tutorialText.text = loadingTexts[index];
+		//TODO: add picture too
+	}
+	private void Configure()
     {
         // Save the bar fill's initial local scale:
         barFillLocalScale = barFillRectTransform.localScale;
