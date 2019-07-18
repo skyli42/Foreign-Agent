633c83ff6c5c043f9351238303f85ee4a972d675
AidanBrasseur <aidanbrasseur@gmail.com>
2019-07-18 19:08:30 -0400

    Fixed B cell intro bug

--- Foreign Agent/Assets/Scripts/BIntroController.cs
+++ Foreign Agent/Assets/Scripts/BIntroController.cs
@@ -6,69 +6,68 @@ using UnityEngine.SceneManagement;
 using UnityEngine.AI;
 public class BIntroController : MonoBehaviour
 {
-	public GameObject player;
-	public RPGTalk introTalk;
-	public RPGTalk endTalk;
-	private Animator m_Animator;
-	public GameObject frozenMacrophage;
-	public Image grayScreen;
+    public GameObject player;
+    public RPGTalk introTalk;
+    public RPGTalk endTalk;
+    private Animator m_Animator;
+    public GameObject frozenMacrophage;
+    public Image grayScreen;
 
-	private bool endPlayed = false;
-	public GameObject endCell;
-	public GameObject firstCell;
-	public GameObject endMenu;
-	public AudioSource victorySound;
-	private bool cellCaptured = false;
-	private Vector3 frozenPos;
-	void Start()
-	{
-		m_Animator = player.GetComponent<Animator>();
-		grayScreen.enabled = true;
-		frozenPos = frozenMacrophage.GetComponent<Transform>().position;
-	}
-	public void CancelControls()
-	{
-		player.GetComponent<PlayerMovement>().dashStart = false;
-		player.GetComponent<PlayerMovement>().enabled = false;
-		player.GetComponent<companionSpawn>().enabled = false;
+    private bool endPlayed = false;
+    public GameObject firstCell;
+    public GameObject endMenu;
+    public AudioSource victorySound;
+    private bool cellCaptured = false;
+    private Vector3 frozenPos;
+    void Start()
+    {
+        m_Animator = player.GetComponent<Animator>();
+        grayScreen.enabled = true;
+        frozenPos = frozenMacrophage.GetComponent<Transform>().position;
+    }
+    public void CancelControls()
+    {
+        player.GetComponent<PlayerMovement>().dashStart = false;
+        player.GetComponent<PlayerMovement>().enabled = false;
+        player.GetComponent<companionSpawn>().enabled = false;
 
-		m_Animator.SetBool("IsWalking", false);
-		m_Animator.SetBool("IsRunning", false);
-		grayScreen.enabled = true;
-	}
+        m_Animator.SetBool("IsWalking", false);
+        m_Animator.SetBool("IsRunning", false);
+        grayScreen.enabled = true;
+    }
 
-	//give back the controls to player
-	public void GiveBackControls()
-	{
-		player.GetComponent<PlayerMovement>().enabled = true;
-		player.GetComponent<companionSpawn>().enabled = true;
-		grayScreen.enabled = false;
-	}
-	void LateUpdate()
-	{	
-		if(!cellCaptured && firstCell.GetComponentInChildren<CellCapture>().capped)
-		{
-			cellCaptured = true;
-		}
-		if (!cellCaptured)
-		{
-			frozenMacrophage.GetComponent<Transform>().position = frozenPos;
-		}
-		if (!endPlayed && endCell.GetComponentInChildren<CellCapture>().capped)
-		{
-			CancelControls();
-			endMenu.SetActive(false);
-			Time.timeScale = 1.0f;
-			endTalk.NewTalk("endLevelStart", "endLevelEnd", endTalk.txtToParse);
-			endPlayed = true;
-		}
-	}
-	public void activateEndMenu()
-	{
-		victorySound.Play();
-		endMenu.SetActive(true);
-		Time.timeScale = 0f;
-	}
+    //give back the controls to player
+    public void GiveBackControls()
+    {
+        player.GetComponent<PlayerMovement>().enabled = true;
+        player.GetComponent<companionSpawn>().enabled = true;
+        grayScreen.enabled = false;
+    }
+    void LateUpdate()
+    {
+        if (!cellCaptured && firstCell.GetComponentInChildren<CellCapture>().capped)
+        {
+            cellCaptured = true;
+        }
+        if (!cellCaptured)
+        {
+            frozenMacrophage.GetComponent<Transform>().position = frozenPos;
+        }
+        if (!endPlayed && GameController.Instance.numCaptures == GameController.Instance.numCellsInLevel)
+        {
+            CancelControls();
+            endMenu.SetActive(false);
+            Time.timeScale = 1.0f;
+            endTalk.NewTalk("endLevelStart", "endLevelEnd", endTalk.txtToParse);
+            endPlayed = true;
+        }
+    }
+    public void activateEndMenu()
+    {
+        victorySound.Play();
+        endMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
 }
 
 
